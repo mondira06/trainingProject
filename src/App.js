112e1860b90152dfc7d41b0ffdab40dbf63e32d0
ravneet0214 <ravneetkaur.bca.aec@gmail.com>
2024-05-21 19:11:02 +0530

    Users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-<<<<<<< HEAD
-import AmountSetup from "./Components/AmountSetup";
-import ManageWinner from "./Components/ManageWinnerSettings";
-import Withdrawal from "./Components/ManageWithdrawalRequest";
+import AmountSetup from "./Components/Pages/AmountSetup";
+import ManageRole from "./Components/Pages/ManageRole";
+import ManageTask from "./Components/Pages/ManageTask";
+import ManageUser from "./Components/Pages/ManageUser";
+import ManageWinner from "./Components/Pages/ManageWinnerSettings";
+import Withdrawal from "./Components/Pages/ManageWithdrawalRequest";
 // import Navbar from "./Components/Navbar"
 function App() {
   return (
@@ -12,22 +14,6 @@ function App() {
        {/* <AmountSetup/> */}
       {/* <ManageWinner/> */}
       <Withdrawal/>
-=======
-import Navbar from "./Components/MenuItems/Navbar";
-import UserPage from "./Components/UserPage";
-import Dashboard from "./Components/Pages/Dashboard";
-import Rewardsystem from "./Components/Pages/Rewardsystem"
-import Appupi from "./Components/Pages/Appupi";
-function App() {
-  return (
-    <div className="App">
-      {/* <UserPage /> */}
-      {/* <Navbar /> */}
-      {/* <Dashboard /> */}
-      {/* <Rewardsystem /> */}
-      <Appupi/>
-      
->>>>>>> 6e4194b54738c3b812a21de33fa83b51d4028320
     </div>
   );
 }
