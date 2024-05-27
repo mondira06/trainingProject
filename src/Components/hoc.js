1c33be076d33d1463de01633dc1c663f598241b3
Techydeveloper12 <dheeraj4567yadav@gmail.com>
2024-05-27 12:07:34 +0530

    new code

---
+++ src/Components/hoc.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
+export default function withAuth(WrappedComponent) {
+  
+  return (props) => {
+    const token = Cookies.get('token');
+    let location = useLocation();
+    let navigate = useNavigate();
+
+    React.useEffect(() => {
+      if (!token) {
+        navigate('/login', {
+          state: { from: location.pathname }
+        });
+      }
+    }, [location, token, navigate]);
+
+    return <>{token && <WrappedComponent {...props} />}</>
+  }
+}
